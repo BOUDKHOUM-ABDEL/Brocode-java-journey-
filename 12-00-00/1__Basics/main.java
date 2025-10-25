552ef34b3a65d4268e711024f672e59cfb2c2173
BOUDKHOUM-ABDEL <boudkhoum.abdellah.code@gmail.com>
2025-10-25 15:05:34 +0100

    update

--- 12-00-00/1__Basics/main.java
+++ 12-00-00/1__Basics/main.java
@@ -1,6 +1,23 @@
+
 public class main {
     public static void main(String[] args) {
+              //java statments
+        System.out.println("Hello World!");
+        System.out.println("I am learning Java.");
+        System.out.println("It is awesome!");
+            //  Java Output Numbers
+        System.out.println(3);
+        System.out.println(358);
+        System.out.println(50000);
+        System.out.println(2 * 5);
 
-        System.out.println("Hello World");
+           //variables
+        int myNum = 15;
+        myNum = 20;  // myNum is now 20
+        System.out.println(myNum);
+        String firstName = "John ";
+        String lastName = "Doe";
+        String fullName = firstName + lastName;
+        System.out.println(fullName);
     }
 }
